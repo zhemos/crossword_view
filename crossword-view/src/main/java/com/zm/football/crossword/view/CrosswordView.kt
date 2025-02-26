1263a43533b1b7a10af511ddc4d43f6cc907989f
zhemos <moskalenko.16.11.1996@gmail.com>
2025-02-27 01:25:32 +0300

    last commit

--- crossword-view/src/main/java/com/zm/football/crossword/view/CrosswordView.kt
+++ crossword-view/src/main/java/com/zm/football/crossword/view/CrosswordView.kt
@@ -1,7 +1,34 @@
 package com.zm.football.crossword.view
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
-class CrosswordView(context: Context, attrs: AttributeSet?) : View(context, attrs)
+class CrosswordView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+
+    private val logger = Logger(isDebug = true)
+
+    private val paint = Paint().apply {
+        color = Color.BLACK
+        strokeWidth = 5f
+        style = Paint.Style.STROKE
+    }
+    private val paint2 = Paint().apply {
+        color = Color.BLACK
+        style = Paint.Style.FILL
+    }
+
+    private val size = 100f.px
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        logger.log("draw")
+        val left = width
+        canvas.drawRect(16f.px, 32f.px, width - 10f, 100f, paint2)
+    }
+
+    private val Float.px get() = this * context.resources.displayMetrics.density
+}
---
+++ crossword-view/src/main/java/com/zm/football/crossword/view/Logger.kt
@@ -0,0 +1,9 @@
+package com.zm.football.crossword.view
+
+import android.util.Log
+
+internal class Logger(private val isDebug: Boolean = false) {
+    fun log(message: String) {
+        if (isDebug) Log.d("zm1996", message)
+    }
+}
